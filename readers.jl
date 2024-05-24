ccddf4f8d8eea906cfd0f371c1c72e2081eeb387
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2024-05-24 16:59:09 +0530

    diagnosing motion orientation

--- readers.jl
+++ readers.jl
@@ -91,7 +91,7 @@ function read_das_data(pathfilename::AbstractString;
     ncolumn=30 )
 
     # trivially iterate over 1-vector
-    dt, X = read_scs_data(pathfilename[1:1]; 
+    dt, X = read_das_data(pathfilename[1:1];  # changed to read_das_data
         nheader=nheader, nsample=nsample, ncolumn=ncolumn )
     return dt, X
 end
@@ -99,7 +99,7 @@ end
 "read and concatenate data from multiple files"
 function read_das_data(pathfilename::Vector{<:AbstractString};
     nheader=1,
-    nsample=sum( countlines.(pathfilenames) - nheader ),
+    nsample=sum( countlines.(pathfilenames) .- nheader ),
     ncolumn=30 ) # data, not including timestamp
 
     # preallocate the data
@@ -172,95 +172,238 @@ end
 
 end
 
+# GPS and heading-roll readers
 module DasGps
 
-#using Dates
+using Dates
 using ..NoaaDas
-export read_scs_dict
 
-# SCS readers process lat,lon into decimal degrees
+export read_gps_dict, read_hed_dict
 
-"read das data and put in a dictionary"
-function read_scs_dict(pathfilename, keys;
+# parameters for ASTRAL - feed defaults to functions
+baseyear = 2024
+mastdir = "./data/PSL/"
+
+"yearday from Date or DateTime"
+yday(dt) = Dates.value(Date(dt) - Date(year(dt)-1,12,31))
+yday(ddd::AbstractString) = parse(Int, ddd)
+
+"read gps file GPRMC data into a dict"
+function read_gps_dict(pathfilenames;
     nheader=1,
-    nsample=sum(countlines.(pathfilename) .- nheader),
-    ncolumn=length(keys)-1 ) # default columns in data excluding datestamp
+    nsample=sum(countlines.(pathfilenames) .- nheader),
+    ncolumn=7 )
 
-    # read data from file
-    datatime, X = read_scs_data(pathfilename; 
-        nheader=nheader, nsample=nsample, ncolumn=ncolumn )
-    return das_dict(keys, datatime, X) # returns dict
-end
+    # method: psldatetime(d::Date, hr::Integer, psltime::String)
 
-# parse gps lat,lon to decimal degrees
-NSEWsgn(s) = endswith(uppercase(s),r"S|W") ? -1 : 1
-declat(s) = NSEWsgn(s)*( parse(Float32, s[1:2]) + parse(Float32, s[3:end-1])/60 )
-declon(s) = NSEWsgn(s)*( parse(Float32, s[1:3]) + parse(Float32, s[4:end-1])/60 )
-
-parseblank2missing(T, s) = isempty(s) ? missing : parse(T, s)
-
-"read and parse one file"
-function read_scs_data(pathfilename::AbstractString;
-    nheader=1, 
-    nsample=countlines(pathfilenames) - nheader, 
-    ncolumn=26 )
-
-    # trivially iterate over 1-vector
-    dt, X = read_scs_data(pathfilename[1:1]; 
-        nheader=nheader, nsample=nsample, ncolumn=ncolumn )
-    return dt, X
-end
-
-"read and concatenate data from multiple files"
-function read_scs_data(pathfilename::Vector{<:AbstractString};
-    nheader=1,
-    nsample=sum( countlines.(pathfilenames) - nheader ),
-    ncolumn=26 ) # data, not including timestamp
+    # doesn't use das_dict() but it could.
 
     # preallocate the data
-    # psltime = Vector{String}(undef, nsample) # will point to data as it is read
-    psldt = Vector{DateTime}(undef, nsample) # will point to data as it is read
-    X = Array{Union{Float32,Missing}, 2}(undef, nsample, ncolumn)
-    fill!(X, missing)
+    psldt  = Vector{DateTime}(undef, nsample) # will point to data as it is read
+    gpsdt  = Vector{DateTime}(undef, nsample) # will point to data as it is read
+    lat    = Vector{Float64}(undef, nsample)
+    lon    = Vector{Float64}(undef, nsample)
+    speed  = Vector{Float64}(undef, nsample)
+    course = Vector{Float64}(undef, nsample)
 
     nl = 0
-    maxcol=0
-    for pfile in pathfilename
-        # find hour from the filename
-        shortfilename = last(splitpath(pfile))
+    # handle a vector of pathfilenames or a single pathfilename
+    p = pathfilenames isa Vector ? pathfilenames : Ref(pathfilenames)
+    for pathfilename in p
+        # find datetime from the filename
+        shortfilename = last(splitpath(pathfilename))
         ddd = shortfilename[end-12:end-10]
         basedt = Date(baseyear-1,12,31) + Day(yday(ddd))
         hr = parse(Int32, shortfilename[end-9:end-8])
 
-        open(pfile) do file
+        open(pathfilename) do file
             for _ in 1:nheader
                 readline(file) # skip header
             end
             for line in readlines(file)
-                nl += 1
-                splt = split(line, r"[\s,]+")
+                fields = split(line, [' ','\t'])
+                if startswith(fields[2], "\$GPRMC")
+                    nl += 1
+                    psltime = fields[1] # string
+                    psldt[nl] = psldatetime(Date(basedt), hr, psltime)
 
-                nx = min(ncolumn, length(splt[2:end]))
-                if nx > 0 # skip empty lines
-                    psltime = splt[1]
-                    psldt[nl] = psldatetime(basedt, hr, psltime)
-
-                    lat = declat( splt[2] )
-                    lon = declon( splt[3] )
-                    @show splt
-                    dataline = parseblank2missing.(Float32, splt[4:end])
-                    maxcol = max(maxcol, nx) # data in longest line
-
-                    X[nl, 1:nx] .= cat(lat,lon,dataline[1:nx-2], dims=1)
+                    gpsdt[nl], lat[nl], lon[nl], speed[nl], course[nl] = parse_gprmc(fields[2])
                 end
             end
         end
     end
 
-    return psldt[1:nl], X[1:nl, 1:maxcol]
+    # Return a dictionary with the parsed fields
+    D = Dict{Symbol, Any}()
+    # DateTime
+    D[:time]     = psldt[1:nl]
+    D[:gpstime]  = gpsdt[1:nl]
+    # Float
+    D[:lat]      = lat[1:nl]
+    D[:lon]      = lon[1:nl]
+    D[:sog_kts]  = speed[1:nl]
+    D[:cog_deg]  = course[1:nl]
+
+    return D
 end
 
+"""
+Parse one GPRMC NMEA message.
+
+Arguments:
+- `message::String`: GPRMC NMEA message
+
+Returns:
+- A dictionary containing the parsed fields
+"""
+function parse_gprmc(message::AbstractString)
+    # Split the message into fields
+    fields = split(message, ",")
+    
+    # Check that the message is a GPRMC message
+    # and position is valid.
+    if fields[1] != "\$GPRMC"
+        error("Not a GPRMC message")
+    elseif fields[3] != "A"
+        error("Invalid position status")
+    end
+    
+    # Extract the fields
+    time_str = fields[2]
+    # status = fields[3]
+    lat_str = fields[4]
+    lat_dir = fields[5]
+    lon_str = fields[6]
+    lon_dir = fields[7]
+    speed_str = fields[8]
+    course_str = fields[9]
+    date_str = fields[10]
+    
+    # Parse the time (hhmmss.sss) and date (ddmmyy)
+    time = tryparse(Time, time_str[1:6], dateformat"HHMMSS")
+    date = tryparse(Date, date_str, dateformat"ddmmyy")
+
+    # Combine date and time into a DateTime object
+    gpstime = try 
+        DateTime(date, time) 
+    catch 
+        @warn "Invalid date or time format"
+    end
+
+    # Parse latitude and longitude in degrees and minutes (ddmm.mmmm)
+    lat_deg = tryparse(Float64, lat_str[1:2])
+    lat_min = tryparse(Float64, lat_str[3:end])
+    lat = lat_deg + lat_min / 60
+    if lat_dir == "S"
+        lat = -lat
+    end
+
+    lon_deg = tryparse(Float64, lon_str[1:3])
+    lon_min = tryparse(Float64, lon_str[4:end])
+    lon = lon_deg + lon_min / 60
+    if lon_dir == "W"
+        lon = -lon
+    end
+
+    # Parse speed (knots) and course (degrees)
+    speed = tryparse(Float64, speed_str)
+    course = tryparse(Float64, course_str)
+
+    # Return a dictionary with the parsed fields
+    return gpstime, lat, lon, speed, course
 end
+# Example usage
+# message = "\$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A"
+# parsed_data = parse_gprmc(message)
+# println(parsed_data)
+
+# "read and parse one hed data file"
+# function read_hed_data(pathfilename::AbstractString;
+#     nheader=1,
+#     nsample=countlines(pathfilenames) - nheader, 
+#     ncolumn=30 )
+
+#     # trivially iterate over 1-vector
+#     dt, X = read_hed_data(pathfilename[1:1]; 
+#         nheader=nheader, nsample=nsample, ncolumn=ncolumn )
+#     return dt, X
+# end
+
+"read differential gps heading data into a dict"
+# reads into array _and_ puts in dictionary
+function read_hed_dict(pathfilenames;
+    nheader=1,
+    nsample=sum(countlines.(pathfilenames) .- nheader),
+    ncolumn=2 )
+
+    # preallocate the data
+    psldt   = Vector{DateTime}(undef, nsample) # will point to data as it is read
+    heading = Vector{Float32}(undef, nsample)
+    roll    = Vector{Float32}(undef, nsample)
+
+    nl = 0
+    # handle scalars or vectors with 1 line!
+    p = pathfilenames isa Vector ? pathfilenames : Ref(pathfilenames)
+    for pathfilename in p
+        # find datetime from the filename
+        shortfilename = last(splitpath(pathfilename))
+        ddd = shortfilename[end-12:end-10]
+        basedt = Date(baseyear-1,12,31) + Day(yday(ddd))
+        hr = parse(Int32, shortfilename[end-9:end-8])
+        # method: psldatetime(d::Date, hr::Integer, psltime::String)
+
+        open(pathfilename) do file
+            for _ in 1:nheader
+                readline(file) # skip header
+            end
+            for line in readlines(file)
+                fields = split(line, [' ','\t'])
+                if startswith(fields[2], "\$PSAT,HPR")
+                    nl += 1
+                    psltime = fields[1] # string
+                    psldt[nl] = psldatetime(Date(basedt), hr, psltime)
+                    heading[nl], roll[nl] = parse_hpr(fields[2])
+                end
+            end
+        end
+    end
+
+    # Return a dictionary with the parsed fields
+    D = Dict{Symbol, Any}()
+    D[:time]     = psldt[1:nl]    # DateTime
+    D[:heading]  = heading[1:nl]    # Float
+    D[:roll]     = roll[1:nl]
+    return D
+end
+
+"""
+Parse a PSAT,HPR NMEA message of the form
+\$PSAT,HPR,000002.00,184.19,,-1.73,N*3D
+
+Arguments:  message::String`: PSAT,HPR NMEA message
+Returns:    heading::Float32, roll::Float:32
+"""
+function parse_hpr(message::AbstractString)
+    # example
+    # $PSAT,HPR,000002.00,184.19,,-1.73,N*3D
+
+    # Split the message into fields
+    fields = split(message, ",")
+    
+    # Check that the message is a PSAT message
+    if !startswith(fields[1], "\$PSAT") || !startswith(fields[2], "HPR")
+        error("Not a $PSAT,HPR message")
+    end
+
+    # Parse heading and roll
+    heading_deg = tryparse(Float32, fields[4])
+    roll_deg    = tryparse(Float32, fields[6])
+    
+    # Return a dictionary with the parsed fields
+    return heading_deg, roll_deg
+end
+
+end # DasGps
 
 module DasScs
 
@@ -308,7 +451,7 @@ end
 "read and concatenate data from multiple files"
 function read_scs_data(pathfilename::Vector{<:AbstractString};
     nheader=1,
-    nsample=sum( countlines.(pathfilenames) - nheader ),
+    nsample=sum( countlines.(pathfilenames) .- nheader ),
     ncolumn=26 ) # data, not including timestamp
 
     # preallocate the data
